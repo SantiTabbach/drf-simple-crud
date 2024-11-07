64f23ea742d3a842d92b49d5ba18f412a20a1ee7
Santi Tabbach <tabbach97@gmail.com>
2024-11-07 10:51:05 -0300

    Add Task model

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 
-# Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField()
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="tasks"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title

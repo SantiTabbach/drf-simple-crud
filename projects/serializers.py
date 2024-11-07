defdbc32fac61883e0b6e8f16ecdab2d09a67612
Santi Tabbach <tabbach97@gmail.com>
2024-11-07 10:52:30 -0300

    Add task serializer and update project serializer with related tasks

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,9 +1,36 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "id",
+            "title",
+            "description",
+            "completed",
+            "created_at",
+            "project",
+        ]
+        read_only_fields = [
+            "created_at",
+        ]
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
-        fields = ("id", "title", "description", "technology", "created_at")
-        read_only_fields = ("created_at",)
+        fields = [
+            "id",
+            "title",
+            "description",
+            "technology",
+            "created_at",
+            "tasks",
+        ]
+        read_only_fields = [
+            "created_at",
+        ]

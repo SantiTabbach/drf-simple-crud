8c81afcbb45577fa583f28f0ce2f295df1e039d1
Santi Tabbach <tabbach97@gmail.com>
2024-11-07 10:52:05 -0300

    Move viewsets from api to views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Project, Task
+from rest_framework import viewsets, permissions
+from .serializers import ProjectSerializer, TaskSerializer
 
-# Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TaskSerializer
